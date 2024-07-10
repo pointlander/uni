0ff33d9be054722356e05b3c7b22db38814e5923
Andrew Snodgrass <pointlander@gmail.com>
2024-07-10 17:28:24 -0500

    Maximize variance

--- main.go
+++ main.go
@@ -38,4 +38,41 @@ func main() {
 		}
 		fmt.Println()
 	}
+
+	rng1 := matrix.Rand(1)
+	optimizer := matrix.NewOptimizer(&rng1, 9, .1, 6, func(samples []matrix.Sample, x ...matrix.Matrix) {
+		for i, sample := range samples {
+			x1 := sample.Vars[0][0].Sample()
+			y1 := sample.Vars[0][1].Sample()
+			z1 := sample.Vars[0][2].Sample()
+			w1 := x1.Add(y1.H(z1))
+
+			input := matrix.NewZeroMatrix(3, 1)
+			for i := range input.Data {
+				input.Data[i] = w1.Data[i]
+			}
+			for i := 0; i < 8; i++ {
+				output := matrix.SelfAttention(query.MulT(input), key.MulT(input), value.MulT(input))
+				newInput := matrix.NewMatrix(input.Cols, input.Rows+output.Rows)
+				newInput.Data = append(newInput.Data, input.Data...)
+				newInput.Data = append(newInput.Data, output.Data...)
+				input = newInput
+			}
+			mean, variance := float32(0.0), float32(0.0)
+			for _, value := range input.Data {
+				mean += value
+			}
+			mean /= float32(len(input.Data))
+			for _, value := range input.Data {
+				diff := mean - value
+				variance += diff * diff
+			}
+			variance /= float32(len(input.Data))
+			samples[i].Cost = -float64(variance)
+		}
+	}, matrix.NewCoord(3, 1))
+	for i := 0; i < 33; i++ {
+		sample := optimizer.Iterate()
+		fmt.Println(i, sample.Cost)
+	}
 }
